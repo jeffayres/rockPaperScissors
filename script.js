d3273e9b45a4dcf984a222371d1b42a0cb6a4417
jeffa <92555037+jayres3@users.noreply.github.com>
2024-06-21 15:18:17 -0700

    Updated RockPaperScissors with UI

--- script.js
+++ script.js
@@ -1,78 +1,130 @@
-const gameChoices = ["rock", "paper", "scissors"];
-
-function playGame() {
+// Complete logic of game inside this function
+const game = () => {
+  let playerScore = 0;
   let computerScore = 0;
-  let humanScore = 0;
+  let moves = 0;
 
-  for (let i = 0; i < 5; i++) {
-    playRound();
+
+  // Function to 
+  const playGame = () => {
+      const rockBtn = document.querySelector('.rock');
+      const paperBtn = document.querySelector('.paper');
+      const scissorBtn = document.querySelector('.scissor');
+      const playerOptions = [rockBtn, paperBtn, scissorBtn];
+      const computerOptions = ['rock', 'paper', 'scissors']
+
+      // Function to start playing game
+      playerOptions.forEach(option => {
+          option.addEventListener('click', function () {
+
+              const movesLeft = document.querySelector('.movesleft');
+              moves++;
+              movesLeft.innerText = `Moves Left: ${10 - moves}`;
+
+
+              const choiceNumber = Math.floor(Math.random() * 3);
+              const computerChoice = computerOptions[choiceNumber];
+
+              // Function to check who wins
+              winner(this.innerText, computerChoice)
+
+              // Calling gameOver function after 10 moves
+              if (moves == 10) {
+                  gameOver(playerOptions, movesLeft);
+              }
+          })
+      })
+
   }
-  console.log("Player: %s to Computer: %s", humanScore, computerScore);
 
-  function playRound() {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+  // Function to decide winner
+  const winner = (player, computer) => {
+      const result = document.querySelector('.result');
+      const playerScoreBoard = document.querySelector('.p-count');
+      const computerScoreBoard = document.querySelector('.c-count');
+      player = player.toLowerCase();
+      computer = computer.toLowerCase();
+      if (player === computer) {
+          result.textContent = 'Tie'
+      }
+      else if (player == 'rock') {
+          if (computer == 'paper') {
+              result.textContent = 'Computer Won';
+              computerScore++;
+              computerScoreBoard.textContent = computerScore;
 
-    switch (humanChoice) {
-      case "rock":
-        if (computerChoice === "rock") {
-          tieScenario();
-        } else if (computerChoice === "paper") {
-          lossScenario();
-        } else {
-          winScenario();
-        }
-        break;
-      case "paper":
-        if (computerChoice === "rock") {
-          winScenario();
-        } else if (computerChoice === "paper") {
-          tieScenario();
-        } else {
-          lossScenario();
-        }
-        break;
-      case "scissors":
-        if (computerChoice === "rock") {
-          lossScenario();
-        } else if (computerChoice === "paper") {
-          winScenario();
-        } else {
-          tieScenario();
-        }
-        break;
-      default:
-        console.log("Invalid choice");
-        break;
-    }
-
-    function winScenario() {
-      console.log("You win! %s beats %s", humanChoice, computerChoice);
-      humanScore++;
-    }
-
-    function lossScenario() {
-      console.log("You lose! %s beats %s", computerChoice, humanChoice);
-      computerScore++;
-    }
-
-    function tieScenario() {
-      console.log("Tie!");
-    }
+          } else {
+              result.textContent = 'Player Won'
+              playerScore++;
+              playerScoreBoard.textContent = playerScore;
+          }
+      }
+      else if (player == 'scissors') {
+          if (computer == 'rock') {
+              result.textContent = 'Computer Won';
+              computerScore++;
+              computerScoreBoard.textContent = computerScore;
+          } else {
+              result.textContent = 'Player Won';
+              playerScore++;
+              playerScoreBoard.textContent = playerScore;
+          }
+      }
+      else if (player == 'paper') {
+          if (computer == 'scissors') {
+              result.textContent = 'Computer Won';
+              computerScore++;
+              computerScoreBoard.textContent = computerScore;
+          } else {
+              result.textContent = 'Player Won';
+              playerScore++;
+              playerScoreBoard.textContent = playerScore;
+          }
+      }
   }
+
+  // Function to run when game is over
+  const gameOver = (playerOptions, movesLeft) => {
+
+      const chooseMove = document.querySelector('.move');
+      const result = document.querySelector('.result');
+      const reloadBtn = document.querySelector('.reload');
+
+      playerOptions.forEach(option => {
+          option.style.display = 'none';
+      })
+
+
+      chooseMove.innerText = 'Game Over!!'
+      movesLeft.style.display = 'none';
+
+      if (playerScore > computerScore) {
+          result.style.fontSize = '2rem';
+          result.innerText = 'You Won The Game'
+          result.style.color = '#308D46';
+      }
+      else if (playerScore < computerScore) {
+          result.style.fontSize = '2rem';
+          result.innerText = 'You Lost The Game';
+          result.style.color = 'red';
+      }
+      else {
+          result.style.fontSize = '2rem';
+          result.innerText = 'Tie';
+          result.style.color = 'grey'
+      }
+      reloadBtn.innerText = 'Restart';
+      reloadBtn.style.display = 'flex'
+      reloadBtn.addEventListener('click', () => {
+          window.location.reload();
+      })
+  }
+
+
+  // Calling playGame function inside game
+  playGame();
+
 }
 
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  console.log("Computer chose: ", gameChoices[randomNumber]);
-  return gameChoices[randomNumber];
-}
-
-function getHumanChoice() {
-  let humanChoice = prompt("Choose between rock, paper, or scissors");
-  humanChoice = humanChoice.toLowerCase();
-  console.log("Player chose:", humanChoice);
-  return humanChoice;
-}
-
-playGame();
+// Calling the game function
+game();
